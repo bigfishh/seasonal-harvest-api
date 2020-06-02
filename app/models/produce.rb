f5efbca025d3edc413caf858b3f0dfa2422654fc
bigfishh <anniezheng.anniee@gmail.com>
2020-06-02 19:06:13 -0400

    added associations

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -1,2 +1,4 @@
 class Produce < ApplicationRecord
+    has_many :seasonal_produces, dependent: :destroy
+    has_many :seasons, through: :seasonal_produces
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -1,2 +1,4 @@
 class Season < ApplicationRecord
+    has_many: seasonal_produces, dependent: :destroy
+    has_many: produces, through: :seasonal_produces
 end
